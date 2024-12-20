aa3b70cd9ebe1a11e150b1fffca4c77737bce88e
Eddy200408 <edycoutinho04@gmail.com>
2024-12-20 14:20:53 -0100

    first commit

---
+++ valueIterationAgents.py
@@ -0,0 +1,165 @@
+# valueIterationAgents.py
+# -----------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+# valueIterationAgents.py
+# -----------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+import mdp, util
+
+from learningAgents import ValueEstimationAgent
+import collections
+
+
+class ValueIterationAgent(ValueEstimationAgent):
+    """
+        * Please read learningAgents.py before reading this.*
+
+        A ValueIterationAgent takes a Markov decision process
+        (see mdp.py) on initialization and runs value iteration
+        for a given number of iterations using the supplied
+        discount factor.
+    """
+    def __init__(self, mdp, discount = 0.9, iterations = 100):
+        """
+          Your value iteration agent should take an mdp on
+          construction, run the indicated number of iterations
+          and then act according to the resulting policy.
+
+          Some useful mdp methods you will use:
+              mdp.getStates()
+              mdp.getPossibleActions(state)
+              mdp.getTransitionStatesAndProbs(state, action)
+              mdp.getReward(state, action, nextState)
+              mdp.isTerminal(state)
+        """
+        self.mdp = mdp
+        self.discount = discount
+        self.iterations = iterations
+        self.values = util.Counter() # A Counter is a dict with default 0
+        self.runValueIteration()
+
+
+    def runValueIteration(self):
+        """
+        terminal states have no reward, set val to 0
+        self.values is a dictionary of {s : q_val, ....}
+        I want to update the values associated with s
+
+         """
+        "*** YOUR CODE HERE ***"
+
+        # k iterations
+        for _ in range(self.iterations):
+            update_values_dict = self.values.copy()
+
+            # get Q_values for each possible s_prime
+            for state in self.mdp.getStates():
+                Q_values = [float('-inf')]
+                terminal_state = self.mdp.isTerminal(state)  # boolean
+
+                # Terminal states have 0 value.
+                if terminal_state:
+                    update_values_dict[state] = 0
+
+                else:
+                    legal_actions = self.mdp.getPossibleActions(state)
+
+                    for action in legal_actions:
+                        Q_values.append(self.getQValue(state, action))
+
+                    # update value function at state s to largest Q_value
+                    update_values_dict[state] = max(Q_values)
+
+            self.values = update_values_dict
+
+
+    def getValue(self, state):
+        """
+          Return the value of the state (computed in __init__).
+
+        """
+        return self.values[state]
+
+
+    def computeQValueFromValues(self, state, action):
+        """
+          Compute the Q-value of action in state from the
+          value function stored in self.values.
+          Q-Values: expected future utility from a q-state (chance node)
+          Q*(s, a) = sum_s' of T(s, a, s') [R(s, a, s') + gamma V*(s')]
+        """
+        "*** YOUR CODE HERE ***"
+        possible_next_states = self.mdp.getTransitionStatesAndProbs(state, action)  # [(s_prime, prob), ...]
+        value_utilities = []
+
+        for s_prime, T in possible_next_states:
+            R = self.mdp.getReward(state, action, s_prime)
+            gamma_V = self.discount * self.values[s_prime]
+            value_utilities.append(T * (R + gamma_V))
+
+        return sum(value_utilities)
+
+
+    def computeActionFromValues(self, state):
+        """
+          The policy is the best action in the given state
+          according to the values currently stored in self.values.
+
+          You may break ties any way you see fit.  Note that if
+          there are no legal actions, which is the case at the
+          terminal state, you should return None.
+
+          This is policy Extraction
+          V*(s) = max_a Q*(s, a)
+          return the action with the highest Q-value
+        """
+        "*** YOUR CODE HERE ***"
+        legal_actions = self.mdp.getPossibleActions(state)
+
+        if len(legal_actions) == 0:
+            return None
+
+        actions_qvals = []  # [(action, qval) ... ]
+
+        for action in legal_actions:
+            qval = self.getQValue(state, action)
+            actions_qvals.append((action, qval))
+
+        action_with_best_qval = max(actions_qvals, key=lambda x: x[1])[0]
+        return action_with_best_qval
+
+
+    def getPolicy(self, state):
+        return self.computeActionFromValues(state)
+
+
+    def getAction(self, state):
+        "Returns the policy at the state (no exploration)."
+        return self.computeActionFromValues(state)
+
+
+    def getQValue(self, state, action):
+        return self.computeQValueFromValues(state, action)
+
